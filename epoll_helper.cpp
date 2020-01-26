759a9d57622e34489178da439578f09bec919e01
ttzztztz <ttzztztz@outlook.com>
2020-01-26 21:47:27 +0800

    epoll io

---
+++ epoll_helper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 杨子越 on 2020/1/26.
+//
+
+#include "epoll_helper.h"
---
+++ epoll_helper.h
@@ -0,0 +1,14 @@
+//
+// Created by 杨子越 on 2020/1/26.
+//
+
+#ifndef WEB_SERVER_EPOLL_HELPER_H
+#define WEB_SERVER_EPOLL_HELPER_H
+
+
+class epoll_helper {
+
+};
+
+
+#endif //WEB_SERVER_EPOLL_HELPER_H
